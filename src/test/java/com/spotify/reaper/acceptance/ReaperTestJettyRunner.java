ee5acdc94adfe9f0d873a0d38f047683d4e301d9
Hannu Varjoranta <varjo@spotify.com>
2015-02-04 13:52:52 +0100

    some refactoring on acceptance tests

--- src/test/java/com/spotify/reaper/acceptance/ReaperTestJettyRunner.java
+++ src/test/java/com/spotify/reaper/acceptance/ReaperTestJettyRunner.java
@@ -5,6 +5,7 @@ import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
 
+import com.spotify.reaper.AppContext;
 import com.spotify.reaper.ReaperApplication;
 import com.spotify.reaper.ReaperApplicationConfiguration;
 import com.sun.jersey.api.client.Client;
@@ -22,14 +23,10 @@ import java.net.URI;
 import java.net.URL;
 import java.util.Map;
 
-import javax.ws.rs.core.Response;
-
 import io.dropwizard.cli.ServerCommand;
 import io.dropwizard.lifecycle.ServerLifecycleListener;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Simple Reaper application runner for testing purposes.
@@ -42,11 +39,11 @@ public class ReaperTestJettyRunner {
 
   private static ReaperTestJettyRunner runnerInstance;
 
-  public static void setup() throws Exception {
+  public static void setup(AppContext testContext) throws Exception {
     if (runnerInstance == null) {
       String testConfigPath = Resources.getResource("cassandra-reaper-at.yaml").getPath();
       LOG.info("initializing ReaperTestJettyRunner with config in path: " + testConfigPath);
-      runnerInstance = new ReaperTestJettyRunner(testConfigPath);
+      runnerInstance = new ReaperTestJettyRunner(testConfigPath, testContext);
       runnerInstance.start();
       // Stop the testing Reaper after tests are finished.
       Runtime.getRuntime().addShutdownHook(new Thread() {
@@ -58,8 +55,8 @@ public class ReaperTestJettyRunner {
     }
   }
 
-  public static void callAndExpect(String httpMethod, String urlPath,
-                                   Optional<Map<String, String>> params, int statusCode) {
+  public static ClientResponse callReaper(String httpMethod, String urlPath,
+                                          Optional<Map<String, String>> params) {
     assert runnerInstance != null : "service not initialized, call setup() first";
     String reaperBase = "http://localhost:" + runnerInstance.getLocalPort() + "/";
     URI uri;
@@ -84,21 +81,17 @@ public class ReaperTestJettyRunner {
     } else {
       throw new RuntimeException("Invalid HTTP method: " + httpMethod);
     }
-
-    assertEquals(statusCode, response.getStatus());
-  }
-
-  public static void callAndExpect(String httpMethod, String url,
-                                   Optional<Map<String, String>> params, Response.Status status) {
-    callAndExpect(httpMethod, url, params, status.getStatusCode());
+    return response;
   }
 
   private final String configPath;
 
   private Server jettyServer;
+  private AppContext context;
 
-  public ReaperTestJettyRunner(String configPath) {
+  public ReaperTestJettyRunner(String configPath, AppContext context) {
     this.configPath = configPath;
+    this.context = context;
   }
 
   public void start() {
@@ -106,7 +99,12 @@ public class ReaperTestJettyRunner {
       return;
     }
     try {
-      ReaperApplication reaper = new ReaperApplication();
+      ReaperApplication reaper;
+      if (context != null) {
+        reaper = new ReaperApplication(context);
+      } else {
+        reaper = new ReaperApplication();
+      }
 
       final Bootstrap<ReaperApplicationConfiguration> bootstrap =
           new Bootstrap<ReaperApplicationConfiguration>(reaper) {
