0ed38f717b3af45004c38529d160dfc147afc7b8
Björn Hegerfors <bj0rn@spotify.com>
2015-02-09 12:09:17 +0100

    First draft of RepairSchedule duplicating fields from RepairRun

---
+++ src/main/java/com/spotify/reaper/core/RepairSchedule.java
@@ -0,0 +1,176 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.spotify.reaper.core;
+
+import org.apache.cassandra.repair.RepairParallelism;
+import org.joda.time.DateTime;
+
+public class RepairSchedule {
+
+  private final long repairUnitId;
+  private final State state;
+  private final int daysBetween;
+  private final DateTime nextActivation;
+  private final int segmentCount;
+  private final RepairParallelism repairParallelism;
+  private final String owner;
+  private final DateTime creationTime;
+  private final DateTime pauseTime;
+  private final String lastEvent;
+
+  private RepairSchedule(Builder builder) {
+    this.repairUnitId = builder.repairUnitId;
+    this.state = builder.state;
+    this.daysBetween = builder.daysBetween;
+    this.nextActivation = builder.nextActivation;
+    this.segmentCount = builder.segmentCount;
+    this.repairParallelism = builder.repairParallelism;
+    this.owner = builder.owner;
+    this.creationTime = builder.creationTime;
+    this.pauseTime = builder.pauseTime;
+    this.lastEvent = builder.lastEvent;
+  }
+
+  public long getRepairUnitId() {
+    return repairUnitId;
+  }
+
+  public State getState() {
+    return state;
+  }
+
+  public int getDaysBetween() {
+    return daysBetween;
+  }
+
+  public DateTime getNextActivation() {
+    return nextActivation;
+  }
+
+  public int getSegmentCount() {
+    return segmentCount;
+  }
+
+  public RepairParallelism getRepairParallelism() {
+    return repairParallelism;
+  }
+
+  public String getOwner() {
+    return owner;
+  }
+
+  public DateTime getCreationTime() {
+    return creationTime;
+  }
+
+  public DateTime getPauseTime() {
+    return pauseTime;
+  }
+
+  public String getLastEvent() {
+    return lastEvent;
+  }
+
+  public Builder with() {
+    return new Builder(this);
+  }
+
+  public enum State {
+    RUNNING,
+    PAUSED
+  }
+
+  public static class Builder {
+
+    public final long repairUnitId;
+    private State state;
+    private int daysBetween;
+    private DateTime nextActivation;
+    private int segmentCount;
+    private RepairParallelism repairParallelism;
+    private String owner;
+    private DateTime creationTime;
+    private DateTime pauseTime;
+    private String lastEvent = "no events";
+
+    public Builder(long repairUnitId, State initialState, DateTime creationTime,
+        int segmentCount, RepairParallelism repairParallelism) {
+      this.repairUnitId = repairUnitId;
+      this.state = initialState;
+      this.creationTime = creationTime;
+      this.segmentCount = segmentCount;
+      this.repairParallelism = repairParallelism;
+    }
+
+    private Builder(RepairSchedule original) {
+      repairUnitId = original.repairUnitId;
+      state = original.state;
+      creationTime = original.creationTime;
+      owner = original.owner;
+      pauseTime = original.pauseTime;
+      lastEvent = original.lastEvent;
+      segmentCount = original.segmentCount;
+      repairParallelism = original.repairParallelism;
+    }
+
+    public Builder runState(State state) {
+      this.state = state;
+      return this;
+    }
+
+    public Builder daysBetween(int daysBetween) {
+      this.daysBetween = daysBetween;
+      return this;
+    }
+
+    public Builder nextActivation(DateTime nextActivation) {
+      this.nextActivation = nextActivation;
+      return this;
+    }
+
+    public Builder segmentCount(int segmentCount) {
+      this.segmentCount = segmentCount;
+      return this;
+    }
+
+    public Builder repairParallelism(RepairParallelism repairParallelism) {
+      this.repairParallelism = repairParallelism;
+      return this;
+    }
+
+    public Builder owner(String owner) {
+      this.owner = owner;
+      return this;
+    }
+
+    public Builder creationTime(DateTime creationTime) {
+      this.creationTime = creationTime;
+      return this;
+    }
+
+    public Builder pauseTime(DateTime pauseTime) {
+      this.pauseTime = pauseTime;
+      return this;
+    }
+
+    public Builder lastEvent(String lastEvent) {
+      this.lastEvent = lastEvent;
+      return this;
+    }
+
+    public RepairSchedule build() {
+      return new RepairSchedule(this);
+    }
+  }
+}
