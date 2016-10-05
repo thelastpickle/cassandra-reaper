abd85fae16f520e8d69126a6b5960eb509408460
Jon Haddad <jon@jonhaddad.com>
2016-10-05 15:18:44 -0700

    working on init script

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 mkdir -p build/usr/share/cassandra-reaper
-mkdir -p build/etc/spotify
+mkdir -p build/usr/local/bin
+mkdir -p build/etc/{spotify,init.d}
+
 
 cp resource/cassandra-reaper.yaml build/etc/spotify/
 cp target/cassandra-reaper-0.3.0-SNAPSHOT.jar build/usr/share/cassandra-reaper/
+cp bin/* build/usr/local/bin/
+
 
 fpm -s dir \
 -t deb \
