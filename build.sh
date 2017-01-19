c966385d135b3c56270c717c63170f05ffff6314
Stefan Litsche <stefan.litsche@zalando.de>
2017-01-19 14:14:17 +0100

    Add build script for docker image

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+[ -n "$1" ]
+TAG=pierone.stups.zalan.do/acid/cassandra-reaper:0.3.3-$1
+
+scm-source
+grep -v -e "locally modified" scm-source.json
+
+docker build . -t $TAG
+docker push $TAG
