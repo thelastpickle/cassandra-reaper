ab6b1fccd79db2e6fccb04acbf550f99eccb240e
Joaquin Casares <joaquin@thelastpickle.com>
2017-02-09 20:52:14 -0600

    Don't bind to a specific jar version

--- docker/reaper/docker-entrypoint.sh
+++ docker/reaper/docker-entrypoint.sh
@@ -9,4 +9,4 @@ sed -i'.bak' "s/\[\"127.0.0.1\"\]/[\"$CASSANDRA_HOST\"]/g" \
     ${WORKDIR}/cassandra-reaper/resource/cassandra-reaper-cassandra.yaml
 
 # run Reaper with a Cassandra backend
-java -jar cassandra-reaper-0.4.0-SNAPSHOT.jar server cassandra-reaper/resource/cassandra-reaper-cassandra.yaml
+java -jar cassandra-reaper-*.jar server cassandra-reaper/resource/cassandra-reaper-cassandra.yaml
