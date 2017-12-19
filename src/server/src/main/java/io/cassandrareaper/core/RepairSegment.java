6a78a5cbc0817b73775440777d0742e46739cf35
mck <mick@thelastpickle.com>
2017-12-19 12:44:31 +1100

    Revert master back to 51b971f35fb02111f6e807ccbdf048a475611014  via command line  `git revert  e81c6459266d3c4b0f0627555c1a668112ad7048 6d06781b62d9d0faa2538718fc6e14a6a57683b1 07077d537c49387288a07acacde81c812acfb53e 87cdeaf68f3dad09e658e3da87ad3414bf72d76a 78b0fbd1c107105e362df22dcd7ce45533b1871b`
    This is a combination of 4 commits.
    
    Revert "Fix #291 if endTime is set, so must startTime be set (v1.0.1)"
    This reverts commit e81c6459266d3c4b0f0627555c1a668112ad7048.
    
    Revert "Only disable RepairSegment precondition checks when JVM is running with system property "-Dreaper.disableSegmentChecks=true""
    This reverts commit 6d06781b62d9d0faa2538718fc6e14a6a57683b1.
    
    Revert "Version update in the UI"
    This reverts commit 07077d537c49387288a07acacde81c812acfb53e.
    
    Revert "Release 1.0.2"
    This reverts commit 87cdeaf68f3dad09e658e3da87ad3414bf72d76a.
    
    Revert "Ugly shameful fix for the endTime/startTime infamous bug"
    This reverts commit 78b0fbd1c107105e362df22dcd7ce45533b1871b.

--- src/server/src/main/java/io/cassandrareaper/core/RepairSegment.java
+++ src/server/src/main/java/io/cassandrareaper/core/RepairSegment.java
@@ -18,7 +18,6 @@ import io.cassandrareaper.service.RingRange;
 
 import java.math.BigInteger;
 import java.util.UUID;
-
 import javax.annotation.Nullable;
 
 import com.google.common.base.Preconditions;
