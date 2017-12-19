2723dfbe21d90d7f0ffb0b59e6c77e740e97f495
Anthony Grasso <anthony@thelastpickle.com>
2017-12-19 17:49:51 +1100

    Removed duplicate docker-compose build commands

--- src/ci/before_deploy.sh
+++ src/ci/before_deploy.sh
@@ -11,5 +11,3 @@ cp -R src/packaging/bin cassandra-reaper-${TRAVIS_TAG}/
 cp src/server/target/cassandra-reaper-*.jar cassandra-reaper-${TRAVIS_TAG}/server/target
 cp -R src/packaging/resource cassandra-reaper-${TRAVIS_TAG}/
 tar czf cassandra-reaper-${TRAVIS_TAG}-release.tar.gz cassandra-reaper-${TRAVIS_TAG}/
-docker-compose -f src/packaging/docker-build/docker-compose.yml build > /dev/null
-docker-compose -f src/packaging/docker-build/docker-compose.yml run build > /dev/null
