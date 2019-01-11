815702be20d6cba3e721325a62b24856ca491e31
samy <samy.nospam@gmail.com>
2019-01-11 18:37:26 +0100

    wrong nologin path

--- src/packaging/debian/preinstall.sh
+++ src/packaging/debian/preinstall.sh
@@ -18,5 +18,5 @@ if id -u reaper; then
 echo "skipping user"
 else
 /usr/sbin/groupadd reaper || true
-/usr/sbin/useradd -r -g reaper -s /sbin/nologin -d /var/empty -c 'cassandra reaper' reaper || true
+/usr/sbin/useradd -r -g reaper -s /usr/sbin/nologin -d /var/empty -c 'cassandra reaper' reaper || true
 fi
