914f0f2e78f8431c8088f7d6eca8ec7d8e4fbb7f
mck <mck@apache.org>
2019-08-17 11:29:26 +0200

    Update Copyright year (on files touched this year), and add newline to last line (posix).

--- src/ui/app/jsx/event-screen.jsx
+++ src/ui/app/jsx/event-screen.jsx
@@ -1,6 +1,6 @@
 //
 //  Copyright 2018-2018 Stefan Podkowinski
-//
+//  Copyright 2019-2019 The Last Pickle Ltd
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
