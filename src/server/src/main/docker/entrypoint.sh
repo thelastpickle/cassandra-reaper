ec192b5fa4701d563d6171726d5a6ebbb4253015
Miguel Martinez Pedreira <mimarpe@gmail.com>
2019-10-31 16:31:17 +0100

    Make docker image run as non-root

--- src/server/src/main/docker/entrypoint.sh
+++ src/server/src/main/docker/entrypoint.sh
@@ -17,16 +17,13 @@
 if [ "$1" = 'cassandra-reaper' ]; then
     set -x
 
-    # get around `/usr/local/bin/configure-persistence.sh: line 65: can't create /etc/cassandra-reaper.yml: Interrupted system call` unknown error
-    touch /etc/cassandra-reaper.yml
-
-    su-exec reaper /usr/local/bin/configure-persistence.sh
-    su-exec reaper /usr/local/bin/configure-webui-authentication.sh
-    su-exec reaper /usr/local/bin/configure-metrics.sh
-    exec su-exec reaper java \
-                    ${JAVA_OPTS} \
-                    -jar /usr/local/lib/cassandra-reaper.jar server \
-                    /etc/cassandra-reaper.yml
+    /usr/local/bin/configure-persistence.sh
+    /usr/local/bin/configure-webui-authentication.sh
+    /usr/local/bin/configure-metrics.sh
+    exec java \
+            ${JAVA_OPTS} \
+            -jar /usr/local/lib/cassandra-reaper.jar server \
+            /etc/cassandra-reaper.yml
 fi
 
 exec "$@"
