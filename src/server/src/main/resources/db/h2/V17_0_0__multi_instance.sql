94f79fbef83c695e7b09404702f1bed98903816c
Alexander Dejanovski <alex@thelastpickle.com>
2019-11-28 17:32:04 +0100

    Fix h2 schema and syntax for migrations 17 and 18

--- src/server/src/main/resources/db/h2/V17_0_0__multi_instance.sql
+++ src/server/src/main/resources/db/h2/V17_0_0__multi_instance.sql
@@ -1,29 +1,29 @@
 -- H2-compatible version of multi-instance reaper Postgres DDL
 -- CHANGES:
---    "node" TEXT  -->  "node" VARCHAR(255)  because H2 doesn't support index on TEXT
+--    node VARCHAR  -->  node VARCHAR(255)  because H2 doesn't support index on VARCHAR
 
 CREATE TABLE IF NOT EXISTS leader (
   leader_id BIGINT PRIMARY KEY,
   reaper_instance_id BIGINT,
-  reaper_instance_host TEXT,
+  reaper_instance_host VARCHAR,
   last_heartbeat TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE IF NOT EXISTS running_reapers (
   reaper_instance_id BIGINT PRIMARY KEY,
-  reaper_instance_host TEXT,
+  reaper_instance_host VARCHAR,
   last_heartbeat TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE IF NOT EXISTS node_metrics_v1 (
-  time_partition          BIGINT,
   run_id                  BIGINT,
-  node                    VARCHAR(255),
-  cluster                 TEXT,
-  datacenter              TEXT,
+  ts                      TIMESTAMP WITH TIME ZONE,
+  node                    VARCHAR,
+  cluster                 VARCHAR,
+  datacenter              VARCHAR,
   requested               BOOLEAN,
   pending_compactions     INT,
   has_repair_running      BOOLEAN,
   active_anticompactions  INT,
-  PRIMARY KEY(run_id, time_partition, node)
+  PRIMARY KEY (run_id, ts, node)
 );
