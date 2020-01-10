9081d6c85ad3ea64551b1c0123e6c1e02ff08f3e
Anthony Grasso <anthony@thelastpickle.com>
2020-01-10 15:22:35 +1100

    #823: Update version of react-hot-loader to latest

--- src/ui/webpack.config.js
+++ src/ui/webpack.config.js
@@ -175,7 +175,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         use: [
-          { loader: "react-hot-loader"},
+          { loader: "react-hot-loader/webpack" },
           {
             loader:"babel-loader",
             options: {
