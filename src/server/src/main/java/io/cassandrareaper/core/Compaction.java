0fa59019babd4b0777aa686e2ddb15247d654dbe
Alexander Dejanovski <alex@thelastpickle.com>
2020-08-20 15:20:52 +0200

    Fix header checks

--- src/server/src/main/java/io/cassandrareaper/core/Compaction.java
+++ src/server/src/main/java/io/cassandrareaper/core/Compaction.java
@@ -1,7 +1,7 @@
-/* * Copyright 2018-2018 The Last Pickle Ltd
+/*
+ * Copyright 2018-2018 The Last Pickle Ltd
  *
  *
-
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
