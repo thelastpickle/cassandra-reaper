301d7c6d3fff10ebe151bc23f6855deba2a8dcc1
Alexander Dejanovski <alex@thelastpickle.com>
2020-09-30 07:00:25 +0200

    fix H2 schema

--- src/server/src/main/resources/db/h2/V20_0_0__concurrent_repairs.sql
+++ src/server/src/main/resources/db/h2/V20_0_0__concurrent_repairs.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS running_repairs (
-  repair_id SERIAL UNIQUE,
+  repair_id BIGINT,
   node VARCHAR,
   reaper_instance_host VARCHAR,
-  reaper_instance_id INT,
+  reaper_instance_id BIGINT,
   segment_id INT,
   last_heartbeat TIMESTAMP WITH TIME ZONE,
   PRIMARY KEY(repair_id, node)
