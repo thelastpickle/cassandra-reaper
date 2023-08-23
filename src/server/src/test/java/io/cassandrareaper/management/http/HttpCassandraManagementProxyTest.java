23fb8391f8ef4ea5719554097e54786b556ac183
Michael Burman <michael.burman@datastax.com>
2023-08-23 13:56:52 +0300

    Merge test files after the rebase

--- src/server/src/test/java/io/cassandrareaper/management/http/HttpCassandraManagementProxyTest.java
+++ src/server/src/test/java/io/cassandrareaper/management/http/HttpCassandraManagementProxyTest.java
@@ -18,24 +18,41 @@
 package io.cassandrareaper.management.http;
 
 import io.cassandrareaper.core.Snapshot;
+import io.cassandrareaper.management.RepairStatusHandler;
+import io.cassandrareaper.management.http.models.JobStatusTracker;
 
+import java.math.BigInteger;
 import java.net.InetSocketAddress;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ScheduledExecutorService;
 
+import com.codahale.metrics.MetricRegistry;
+import com.datastax.mgmtapi.client.api.DefaultApi;
 import com.datastax.mgmtapi.client.model.SnapshotDetails;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.cassandra.repair.RepairParallelism;
+import org.apache.commons.lang3.concurrent.ConcurrentUtils;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class HttpCassandraManagementProxyTest {
 
   @Test
   public void testConvertSnapshots() throws Exception {
+    DefaultApi mockClient = Mockito.mock(DefaultApi.class);
+    ScheduledExecutorService executorService = Mockito.mock(ScheduledExecutorService.class);
+    Mockito.doReturn(ConcurrentUtils.constantFuture(null)).when(executorService).submit(any(Callable.class));
     HttpCassandraManagementProxy proxy = new HttpCassandraManagementProxy(
-        null, "/", InetSocketAddress.createUnresolved("localhost", 8080));
+        null, "/", InetSocketAddress.createUnresolved("localhost", 8080), executorService, mockClient);
     SnapshotDetails details = jsonFromResourceFile("example_snapshot_details.json", SnapshotDetails.class);
     List<Snapshot> snapshots = proxy.convertSnapshots(details);
     assertEquals(3, snapshots.size());
@@ -82,5 +99,40 @@ public class HttpCassandraManagementProxyTest {
   private <T extends Object> T jsonFromResourceFile(String filename, Class<T> clazz) throws Exception {
     return new ObjectMapper().readValue(
         this.getClass().getResource(filename).openStream(), clazz);
+  }
+
+  @Test
+  public void testRepairHandlers() throws Exception {
+    HttpManagementConnectionFactory connectionFactory = Mockito.mock(HttpManagementConnectionFactory.class);
+    DefaultApi mockClient = Mockito.mock(DefaultApi.class);
+    Mockito.doReturn("repair-123456789").when(mockClient).repair1(any());
+    ScheduledExecutorService executorService = Mockito.mock(ScheduledExecutorService.class);
+    Mockito.doReturn(ConcurrentUtils.constantFuture(null)).when(executorService).submit(any(Callable.class));
+
+    HttpCassandraManagementProxy httpCassandraManagementProxy = new HttpCassandraManagementProxy(
+        Mockito.mock(MetricRegistry.class), "",
+        Mockito.mock(InetSocketAddress.class), executorService, mockClient);
+    when(connectionFactory.connectAny(any())).thenReturn(httpCassandraManagementProxy);
+
+    RepairStatusHandler repairStatusHandler = Mockito.mock(RepairStatusHandler.class);
+
+    int repairNo = httpCassandraManagementProxy.triggerRepair(BigInteger.ZERO, BigInteger.ONE, "ks",
+        RepairParallelism.PARALLEL,
+        Collections.singleton("table"), true, Collections.emptyList(), repairStatusHandler, Collections.emptyList(), 1);
+
+    assertEquals(123456789, repairNo);
+    assertEquals(1, httpCassandraManagementProxy.jobTracker.size());
+    String jobId = String.format("repair-%d", repairNo);
+    assertTrue(httpCassandraManagementProxy.jobTracker.containsKey(jobId));
+    JobStatusTracker jobStatus = httpCassandraManagementProxy.jobTracker.get(jobId);
+    assertEquals(0, jobStatus.latestNotificationCount.get());
+    assertEquals(1, httpCassandraManagementProxy.repairStatusExecutors.size());
+    assertEquals(1, httpCassandraManagementProxy.repairStatusHandlers.size());
+    assertTrue(httpCassandraManagementProxy.repairStatusHandlers.containsKey(repairNo));
+
+    httpCassandraManagementProxy.removeRepairStatusHandler(repairNo);
+    assertEquals(0, httpCassandraManagementProxy.jobTracker.size());
+    assertEquals(0, httpCassandraManagementProxy.repairStatusExecutors.size());
+    assertEquals(0, httpCassandraManagementProxy.repairStatusHandlers.size());
   }
 }
