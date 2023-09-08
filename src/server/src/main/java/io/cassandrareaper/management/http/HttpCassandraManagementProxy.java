2959018a82ba93e4e632f28d891d5f4fc94ca672
miles-garnsey <miles.garnsey@datastax.com>
2023-09-08 12:09:37 +1000

    Ensure notifications explicitly requested in HTTP triggerRepair.

--- src/server/src/main/java/io/cassandrareaper/management/http/HttpCassandraManagementProxy.java
+++ src/server/src/main/java/io/cassandrareaper/management/http/HttpCassandraManagementProxy.java
@@ -308,6 +308,7 @@ public class HttpCassandraManagementProxy implements ICassandraManagementProxy {
           (new RepairRequest())
               .fullRepair(fullRepair)
               .keyspace(keyspace)
+              .notifications(true)
               .tables(new ArrayList<>(columnFamilies))
               .repairParallelism(RepairRequest.RepairParallelismEnum.fromValue(repairParallelism.getName()))
               .repairThreadCount(repairThreadCount)
